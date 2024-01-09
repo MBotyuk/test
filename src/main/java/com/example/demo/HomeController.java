b9da9caa5591348b34de568bcac8f3f2770abe65
MBotyuk <mbotyuk@gmail.com>
2024-01-09 19:01:39 +0300

    edit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,7 +10,6 @@ public class HomeController
     @RequestMapping("/")
     public String home()
     {
-//        model.addAttribute("users", userRepository.findAll());
         return "index";
     }
 }
